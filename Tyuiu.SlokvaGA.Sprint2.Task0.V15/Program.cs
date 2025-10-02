b0a5953af570cde5d550d3490e1072cd6f7ce164
Tyuiu.SlokvaGA@outlook.com <grislokva@mail.ru>
2025-10-02 17:05:55 +0500

    Add project files.

---
+++ Tyuiu.SlokvaGA.Sprint2.Task0.V15/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.SlokvaGA.Sprint2.Task0.V15.Lib;
+
+namespace Tyuiu.SlokvaGA.Sprint2.Task0.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Слоква Г. А. | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #2                                                              *");
+            Console.WriteLine("* Тема: Операции сравнения                                               *");
+            Console.WriteLine("* Задание #0                                                             *");
+            Console.WriteLine("* Вариант #15                                                            *");
+            Console.WriteLine("* Выполнил: Слоква Г. А. | ИСПб-25-1                                     *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу из операций сравнений                               *");
+            Console.WriteLine("* (==, !=, <, >, <=, >=, последовательность операций не должна           *");
+            Console.WriteLine("* нарушаться) и арифметических выражений, которая вернет логическую      *");
+            Console.WriteLine("* последовательность(массив): (False, False, True, True, False, False),  *");
+            Console.WriteLine("* при x = 3105, y = 275                                         	      	*");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            int x = 3105;
+            int y = 275;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("y = " + y);
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            for (int i=0; i<6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
