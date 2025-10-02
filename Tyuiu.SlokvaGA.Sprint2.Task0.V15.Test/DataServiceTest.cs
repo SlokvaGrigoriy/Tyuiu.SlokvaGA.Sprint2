0f65110e567da24cfd9bdec36092b16c7a9edaf3
Tyuiu.SlokvaGA@outlook.com <grislokva@mail.ru>
2025-10-02 17:08:46 +0500

    Task0V15

--- Tyuiu.SlokvaGA.Sprint2.Task0.V15.Test/DataServiceTest.cs
+++ Tyuiu.SlokvaGA.Sprint2.Task0.V15.Test/DataServiceTest.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.SlokvaGA.Sprint2.Task0.V15.Test
             bool[] res = new bool[6];
             res = ds.GetCompareOperations(x, y);
             bool[] wait = new bool[6] { false, false, true, true, false, false };
-
+            s
             CollectionAssert.AreEqual(wait, res);
         }
     }
