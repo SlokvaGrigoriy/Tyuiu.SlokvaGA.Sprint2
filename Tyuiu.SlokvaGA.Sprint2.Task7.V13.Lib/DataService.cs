c9fc42a14ca0b83490605feb31ecb6bcd96bc88b
Григорий Слоква <Tyuiu.SlokvaGA@outlook.com>
2025-10-10 17:46:23 +0500

    Task7V13

---
+++ Tyuiu.SlokvaGA.Sprint2.Task7.V13.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.SlokvaGA.Sprint2.Task7.V13.Lib
+{
+    public class DataService : ISprint2Task7V13
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+            if ((y <= 1) && (y >= x - 1) && (x * x + y * y <= 1))
+                res = true;
+            else
+                res = false;
+            return res;
+
+        }
+    }
+}
---
+++ Tyuiu.SlokvaGA.Sprint2.Task7.V13.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.SlokvaGA.Sprint2.Task7.V13.Lib;
+namespace Tyuiu.SlokvaGA.Sprint2.Task7.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+
+            double x = 0.8;
+            double y = 0.8;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
